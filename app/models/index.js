15d35ebd5c0e5dc3f8252b2eea645aeb6649413f
Guillaume Munsch <guillaume@zap.lu>
2016-09-21 10:06:32 +0200

    Connection with mysql & example for db.define (ModelInitializer)

---
+++ app/models/index.js
@@ -0,0 +1,5 @@
+import ModelInitializer from './ModelInitializer';
+
+export {
+  ModelInitializer
+};
--- app/server.js
+++ app/server.js
@@ -5,6 +5,8 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import logger from './modules/logger';
+import orm from 'orm';
+import {ModelInitializer} from './models';
 
 var app = express(); // create the app using express
 
@@ -12,6 +14,27 @@ var app = express(); // create the app using express
 var port = 8080;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+const args = process.argv.slice(2);
+
+if (args.length !== 4) {
+  console.log("Usage: npm start 'username' 'password' 'hostname' 'database name'");
+  process.exit(1);
+}
+
+const username = args[0];
+const password = args[1];
+const host = args[2];
+const dbName = args[3];
+// Connection
+const db = orm.connect(`mysql://${username}:${password}@${host}/${dbName}`);
+db.on('connect', function(err) {
+  if (err) return console.error('Connection error: ' + err);
+  const initializer = new ModelInitializer(db);
+  const testModel = initializer.defineTest();
+  console.log(testModel);
+  // connected
+});
 
 // Initialize the routes
 // TODO: test
