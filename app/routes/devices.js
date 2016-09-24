f5aea5e7a4ca955e517c9b6a83ce178d5f7b7990
Guillaume Munsch <guillaume@zap.lu>
2016-09-25 00:00:17 +0200

    Delete Device

--- app/routes/devices.js
+++ app/routes/devices.js
@@ -9,25 +9,39 @@ import httpCode from './../modules/httpCode';
 import dbModels from './../models/DBModels';
 
 class Devices {
-
   get(req, res) {
-    logger.notice("Getting devices");
     dbModels.DeviceModel.all((err, result) => res.json(result));
+    logger.notice("Getting devices");
   }
 
   post(req, res) {
-    console.log(req.body);
-    logger.notice("Adding " + req.body.name + " to devices");
-    dbModels.DeviceModel.create(req.body, function(err, result) {
+    dbModels.DeviceModel.create(req.body, (err, result) => {
       return (err ?
         httpCode.error404(res, 'Error: Bad parameters') :
         httpCode.success(res, req.body.name + " added !")
       );
     });
-    // {
-    //   if ()
-    //   return res.json("OK");
-    // });
+    logger.notice("Adding " + req.body.name + " to devices");
+  }
+
+  update(req, res) {
+
+  }
+
+  delete(req, res) {
+    dbModels.DeviceModel.one({idDevice: req.params.id}, (error, device) => {
+      console.log(device);
+      if (!device) {
+        return httpCode.error400(res, "Device not found");
+      }
+      device.remove(err => {
+        return (err ?
+          httpCode.error500(res, 'Error: Could not remove device') :
+          httpCode.success(res, "Device removed !")
+        );
+      });
+      logger.notice(`Removing device {${req.params.id}}`);
+    });
   }
 
 }
