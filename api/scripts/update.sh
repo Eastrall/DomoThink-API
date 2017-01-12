251be085079f85029116b60a893aca5ee4fc7f30
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2017-01-12 09:51:43 +0100

    Add missing scripts/ folder

---
+++ api/scripts/update.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#
+# Author: Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
+# DomoThink API update script
+#
+
+# define some variables
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+#
+# Log function
+#
+function log {
+  log_type=$1
+  message=$2
+
+  echo -e "${GREEN}${log_type}${NC}: ${message}"
+}
+
+function main {
+  log "info" "updating DomoThink API..."
+
+  sudo /usr/sbin/service domothink stop
+
+  cd /var/domothink
+  git pull origin master # pull from stable release
+  cd /var/domothink/API
+  sudo npm install # try to update node packages
+  sudo npm run-script prestart # compile API
+
+  sudo /usr/sbin/service domothink start
+
+  log "info" "DomoThink API up to date!"
+}
+
+##############
+# MAIN POINT #
+##############
+
+main
