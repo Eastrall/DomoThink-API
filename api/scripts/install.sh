caa3e1be0707994f5ee76cd7ed4441985e2a4a20
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2017-01-17 20:51:30 +0100

    Update install script

--- api/scripts/install.sh
+++ api/scripts/install.sh
@@ -40,10 +40,9 @@ function install_tools {
 #
 function install_node_js {
   log "info" "Installing node.js..."
-  sudo curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+  sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
   sudo apt-get install -y nodejs
   sudo apt-get install -y build-essential
-  sudo apt-get install -y nodejs
   log "info" "Node.js installed!"
 }
 
